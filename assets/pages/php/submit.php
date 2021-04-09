dde20ace6c3304e845c8aa8655f112918e6a2e08
tibthink <bent23jam@gmail.com>
2021-04-09 06:38:50 -0400

    please let this be the one

--- assets/pages/php/submit.php
+++ assets/pages/php/submit.php
@@ -1,20 +1,15 @@
 <?php
-    if (file_exists("signatures.txt")){
-        $file = "signatures.txt";
-    } else {
-        $myfile = fopen("signatures.txt", "a");
-        header("Refresh:0");
-    }
-
-    date_default_timezone_set("America/New_York");
-
-
-    $name = $_POST["StudentName"];
-    $time = date('Y/m/d/h:i:sa');
-
-    file_put_contents($file, $name, " signed on ", $time);
-    fclose($data_file);
-
-    header('Location: '.'/assets/pages/php/signatures.txt');
+        date_default_timezone_set("America/New_York");
+        extract($_REQUEST);
+        $time = date('Y/m/d/h:i:sa').PHP_EOL;
+    
+    
+        $file = fopen("signatures.txt","a");
+    
+        fwrite($file, $StudentName);
+        fwrite($file, " signed on ");
+        fwrite($file, $time . "\r");
+        fclose($file);
+        header('Location: '.'signatures.txt');
 ?>
 
